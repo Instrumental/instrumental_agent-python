78f4d12a3c913ecfd15f89ed9aaf577ca5eafe52
Elijah Miller <elijah.miller@gmail.com>
2016-04-13 13:58:48 -0400

    Cleanup and standardization.

--- test/agent_old.py
+++
@@ -1,7 +0,0 @@
-from instrumental import Agent
-
-a = Agent("4af0fb2451e9d873452d856579a74e7b", collector="localhost:8000", secure=False)
-a.gauge("gauge_test", 1)
-a.gauge("gauge_test", 2.5, 100, 2)
-a.increment("increment_test", 1)
-a.increment("increment_test", 2.5, 100, 2)
